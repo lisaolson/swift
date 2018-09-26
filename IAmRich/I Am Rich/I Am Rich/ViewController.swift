884f98a9cb07e508c6f7feaa0764765cc3aa115d
Lisa Olson <olson.lisa94@gmail.com>
2018-09-26 09:38:18 -0700

    basic I Am Rich App

---
+++ IAmRich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Lisa Olson on 9/24/18.
+//  Copyright © 2018 Lisa Olson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
